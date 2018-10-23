bc54279e6a931ecc0020b06e044b2e89070371a4
148921 <comtruice@gmail.com>
2018-10-23 18:50:36 +0200

    E1: added linting tool with ; and { rule

---
+++ exercise-1.js
@@ -0,0 +1,24 @@
+// 1: semi-colon rule
+function addTwoNumbers(a, b) {
+  return;
+  (a + b);
+}
+console.log('Result:', addTwoNumbers(1, 2));
+
+
+// 2: Curly braces
+
+function myFancyFunction() {
+  return "Important stuff";
+}
+
+function myOtherFunction() {
+  // Do stuff
+}
+
+function myFunctionReturningAnObject() {
+  return {
+    name: "Mad Max",
+    age: 35
+  };
+}
